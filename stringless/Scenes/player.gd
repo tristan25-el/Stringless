bc05773dacd4f8b0e8108cb87fe5349493d4c790
Naryamaa <woxvern@gmail.com>
2026-06-03 16:39:47 +0700

    Dash
    dashing animations

--- stringless/Scenes/player.gd
+++ stringless/Scenes/player.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+var slide_speed: float = 0.08 #dash speed, change if too slow/fast
+var dash_visual_duration: float = 0.22 #visual duration, change if animation too slow/fast
 var current_lane: int = 1
 var hit_line_y: float = 1350.0 
 var move_tween: Tween
@@ -27,15 +29,30 @@ func jump_to_lane(target_lane: int, instant: bool = false) -> void:
 	# block duplicate inputs if player is already in the target lane
 	if target_lane == current_lane:
 		return
-
+		
+	if target_lane > current_lane:
+		sprite.play("dash_right")
+	
+	elif target_lane < current_lane:
+		sprite.play("dash_left")
+		
 	current_lane = target_lane
-
-	# interrupt active movement if a new key is pressed mid-slide
+	
 	if move_tween:
 		move_tween.kill()
 		
-	# slide player horizontally to the new lane
 	move_tween = create_tween()
-	move_tween.tween_property(self, "position", Vector2(target_x, hit_line_y), 0.07)\
+	
+	
+	move_tween.tween_property(self, "position", Vector2(target_x, hit_line_y), slide_speed)\
 		.set_trans(Tween.TRANS_CUBIC)\
 		.set_ease(Tween.EASE_OUT)
+		
+	var visual_hold_time = max(0.0, dash_visual_duration - slide_speed)
+	move_tween.tween_interval(visual_hold_time)
+		
+	
+	move_tween.tween_callback(func(): sprite.play("idle"))
+	
+	
+	
